66e80e4c06c200af24417a83de0a034df4012a7c
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2023-09-05 08:40:16 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -55,11 +55,11 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("Please Wait...⏳")
         try:
             messages = await get_messages(client, ids)
         except:
-            await message.reply_text("Something went wrong..!")
+            await message.reply_text("Something Went Wrong")
             return
         await temp_msg.delete()
 
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("ABOUT", callback_data = "about"),
+                    InlineKeyboardButton("CLOSE", callback_data = "close")
                 ]
             ]
         )
